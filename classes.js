b9ce5b255351679058a9cf31bc25c90277db5191
Joan Njiru <joanwanjirunjiru@gmail.com>
2023-06-27 08:36:33 +0300

    added question

--- classes.js
+++ classes.js
@@ -1,4 +1,12 @@
 
+// 1. **Ancestral Stories:** In many African cultures, the art of storytelling is passed
+// down from generation to generation. Imagine you're creating an application that
+// records these oral stories and translates them into different languages. The
+// stories vary by length, moral lessons, and the age group they are intended for.
+// Think about how you would model `Story`, `StoryTeller`, and `Translator`
+// objects, and how inheritance might come into play if there are different types of
+// stories or storytellers.
+
 class AncestralStories{
     constructor(length,moralLessons,ageGroup,title,language){
         this.length=length;
